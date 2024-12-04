21096ea6372adfaa158cbb81a2f65654225ec44d
Kenny Ketelaar <kenny@ketelaar.tech>
2024-12-04 13:42:25 +0100

    Added part 1 of day 4

--- src/day4/main.ts
+++ src/day4/main.ts
@@ -1,5 +1,57 @@
 export function part1(textFile: string) {
-  return 0;
+  const characterMatrix = textFile
+    .split("\n") // divide into rows
+    .filter((line) => line !== "") // make sure empty lines are not present
+    .map((line) => line.split("")); // divide into columns
+
+  const maxHorIndex = characterMatrix[0].length;
+  const maxVertIndex = characterMatrix.length;
+
+  return characterMatrix.reduce((sum, line, verticalIndex) => {
+    const matchesPerLine = line.map((char, horizontalIndex) => {
+      if (char === "X") {
+        const matches = [
+          findHorizontal(line, horizontalIndex, maxHorIndex),
+          findHorizontalReverse(line, horizontalIndex),
+          findVertical(
+            characterMatrix,
+            horizontalIndex,
+            verticalIndex,
+            maxVertIndex,
+          ),
+          findVerticalReverse(characterMatrix, horizontalIndex, verticalIndex),
+          findDiagonalUpper(
+            characterMatrix,
+            horizontalIndex,
+            verticalIndex,
+            maxHorIndex,
+          ),
+          findDiagonalLower(
+            characterMatrix,
+            horizontalIndex,
+            verticalIndex,
+            maxHorIndex,
+            maxVertIndex,
+          ),
+          findNegativeDiagonalUpper(
+            characterMatrix,
+            horizontalIndex,
+            verticalIndex,
+          ),
+          findNegativeDiagonalLower(
+            characterMatrix,
+            horizontalIndex,
+            verticalIndex,
+            maxVertIndex,
+          ),
+        ];
+        return matches.reduce((sum, bool) => sum + (bool ? 1 : 0), 0);
+      }
+      return 0;
+    });
+
+    return sum + matchesPerLine.reduce((total, value) => total + value);
+  }, 0);
 }
 
 export function part2(textFile: string) {
@@ -20,4 +72,117 @@ export async function main() {
 
 if (import.meta.main) {
   await main();
+}
+
+function findHorizontal(line: string[], index: number, maxHorIndex: number) {
+  if (index + 3 > maxHorIndex) {
+    return false;
+  }
+
+  return line[index + 1] === "M" && line[index + 2] === "A" &&
+    line[index + 3] === "S";
+}
+
+function findHorizontalReverse(line: string[], index: number) {
+  if (index - 3 < 0) {
+    return false;
+  }
+
+  return line[index - 1] === "M" && line[index - 2] === "A" &&
+    line[index - 3] === "S";
+}
+
+function findVertical(
+  matrix: string[][],
+  horizontalIndex: number,
+  verticalIndex: number,
+  maxVertIndex: number,
+) {
+  if (verticalIndex + 3 >= maxVertIndex) {
+    return false;
+  }
+
+  const column = (verticalIndex: number) =>
+    matrix[verticalIndex][horizontalIndex];
+
+  return column(verticalIndex + 1) === "M" &&
+    column(verticalIndex + 2) === "A" && column(verticalIndex + 3) === "S";
+}
+
+function findVerticalReverse(
+  matrix: string[][],
+  horizontalIndex: number,
+  verticalIndex: number,
+) {
+  if (verticalIndex - 3 < 0) return false;
+
+  const column = (verticalIndex: number) =>
+    matrix[verticalIndex][horizontalIndex];
+
+  return column(verticalIndex - 1) === "M" &&
+    column(verticalIndex - 2) === "A" && column(verticalIndex - 3) === "S";
+}
+
+function findDiagonalUpper(
+  matrix: string[][],
+  horizontalIndex: number,
+  verticalIndex: number,
+  maxHorIndex: number,
+) {
+  if (horizontalIndex + 3 >= maxHorIndex || verticalIndex - 3 < 0) {
+    return false;
+  }
+
+  const diagonal = (position: number) =>
+    matrix[verticalIndex - position][horizontalIndex + position];
+
+  return diagonal(1) === "M" && diagonal(2) === "A" && diagonal(3) === "S";
+}
+
+function findDiagonalLower(
+  matrix: string[][],
+  horizontalIndex: number,
+  verticalIndex: number,
+  maxHorIndex: number,
+  maxVertIndex: number,
+) {
+  if (horizontalIndex + 3 >= maxHorIndex || verticalIndex + 3 >= maxVertIndex) {
+    return false;
+  }
+
+  const diagonal = (position: number) =>
+    matrix[verticalIndex + position][horizontalIndex + position];
+
+  return diagonal(1) === "M" && diagonal(2) === "A" && diagonal(3) === "S";
+}
+
+function findNegativeDiagonalUpper(
+  matrix: string[][],
+  horizontalIndex: number,
+  verticalIndex: number,
+) {
+  if (horizontalIndex - 3 < 0 || verticalIndex - 3 < 0) {
+    return false;
+  }
+
+  const diagonal = (position: number) =>
+    matrix[verticalIndex - position][horizontalIndex - position];
+
+  return diagonal(1) === "M" && diagonal(2) === "A" && diagonal(3) === "S";
+}
+
+function findNegativeDiagonalLower(
+  matrix: string[][],
+  horizontalIndex: number,
+  verticalIndex: number,
+  maxVertIndex: number,
+) {
+  if (horizontalIndex - 3 < 0 || verticalIndex + 3 > maxVertIndex) {
+    return false;
+  }
+
+  const diagonal = (position: number) =>
+    matrix[verticalIndex + position][horizontalIndex - position];
+
+  return diagonal(1) === "M" && diagonal(2) === "A" && diagonal(3) === "S";
 }
--- src/day4/main_test.ts
+++ src/day4/main_test.ts
@@ -1,22 +1,30 @@
 import { assertEquals } from "@std/assert/equals";
 import { part1, part2 } from "./main.ts";
 
-const testFile =
-  "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
-const actualFile = await Deno.readTextFile("./src/day3/input.txt");
+const testFile = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`;
+const actualFile = await Deno.readTextFile("./src/day4/input.txt");
 
 Deno.test(function part1ExampleTest() {
-  assertEquals(part1(testFile), -1);
+  assertEquals(part1(testFile), 18);
 });
 
 Deno.test(function part1RealTest() {
-  assertEquals(part1(actualFile), -1);
+  assertEquals(part1(actualFile), 2517);
 });
 
-Deno.test(function part2ExampleTest() {
-  assertEquals(part2(testFile), -1);
-});
+// Deno.test(function part2ExampleTest() {
+//   assertEquals(part2(testFile), -1);
+// });
 
-Deno.test(function part2RealTest() {
-  assertEquals(part2(actualFile), -1);
-});
+// Deno.test(function part2RealTest() {
+//   assertEquals(part2(actualFile), -1);
+// });
